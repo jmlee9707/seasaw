1188a3d151fb4c03f44527dc3141582d7d56070c
jmlee9707 <jmlee9707@naver.com>
2025-03-19 00:41:07 +0900

    styles : globalstyles 세팅, font 세팅

--- client/src/styles/globalStyle.tsx
+++ client/src/styles/globalStyle.tsx
@@ -1,41 +1,60 @@
 import { css } from '@emotion/react';
-// import './font.css';
+import './font.css';
 
 export const globalStyles = css`
 	:root {
 		/* font */
 		--font-NOTO: 'Noto Sans';
+		--font-PRE: 'Pretendard';
 
 		/* primary */
-		--color-primary: #2f2c4d;
-		--color-primary-gray: #4d4d4d;
+		--color-primary: #517cff;
+		--color-primary-900: #0a2476;
+		--color-primary-800: #2654e6;
+		--color-primary-700: #517cff;
+		--color-primary-600: #6d90ff;
+		--color-primary-500: #acc0ff;
+		--color-primary-400: #d1dcff;
+		--color-primary-300: #e3eaff;
+		--color-primary-200: #eff3ff;
+		--color-primary-100: #f4f7ff;
 
 		/* secondary */
-		--color-secondary-80: #595671;
-		--color-secondary-60: #828094;
-		--color-secondary-30: #c1c0ca;
-		--color-secondary-10: #eaeaed;
+		--color-secondary: #ff6a56;
+		--color-secondary-900: #d2230a;
+		--color-secondary-800: #f44a32;
+		--color-secondary-700: #ff6a56;
+		--color-secondary-600: #ff8776;
+		--color-secondary-500: #ff9b8d;
+		--color-secondary-400: #ffc2ba;
+		--color-secondary-300: #ffd4ce;
+		--color-secondary-200: #ffe8e5;
+		--color-secondary-100: #ffefec;
 
 		/* colors */
 
-		--color-success: #608ee5;
-		--color-warning: #b82921;
+		--color-success-600: #00b428;
+		--color-success-900: #018030;
 
-		--color-black: #000000;
+		--color-error-500: #f04349;
+		--color-error-500: #da0000;
+		--color-error-500: #b70202;
+
+		--color-black: #121212;
 		--color-white: #ffffff;
 
 		/* grayscale */
-		--color-gray-900: #1a1a1a;
-		--color-gray-800: #333333;
-		--color-gray-700: #4d4d4d;
-		--color-gray-600: #666666;
-		--color-gray-500: #808080;
-		--color-gray-400: #999999;
-		--color-gray-300: #b2b2b2;
-		--color-gray-200: #cccccc;
-		--color-gray-100: #e5e5e5;
-		--color-gray-50: #f6f8fa;
-		--color-gray-25: #f6f6f6;
+		--color-gray-900: #363840;
+		--color-gray-800: #474b55;
+		--color-gray-700: #595e6a;
+		--color-gray-600: #6b7180;
+		--color-gray-500: #7f8493;
+		--color-gray-400: #9499a5;
+		--color-gray-300: #aeb2bc;
+		--color-gray-200: #c7cad1;
+		--color-gray-100: #dcdee3;
+		--color-gray-50: #eff0f2;
+		--color-gray-25: #fafbfc;
 	}
 
 	* {
